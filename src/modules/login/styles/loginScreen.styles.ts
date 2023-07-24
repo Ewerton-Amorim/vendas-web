4b785a448cf207cd3c9afae6b5c6de9ca61dabd4
Ewerton <ewertonhunter@live.com>
2023-07-24 15:55:56 -0300

    feat: cria component de login

--- src/modules/login/styles/loginScreen.styles.ts
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+export const ContainerLoginScreen = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: right;
+`;
+
 export const BackgroundImage = styled.img`
   position: absolute;
   left: 0;
@@ -7,4 +13,25 @@ export const BackgroundImage = styled.img`
   width: 100%;
   height: 100vh;
   object-fit: cover;
+  z-index: -1;
+`;
+
+export const LogoImage = styled.img``;
+
+export const ContainerLogin = styled.div`
+  display: flex;
+  padding: 22px;
+  justify-content: center;
+  align-items: center;
+  background: #d9d9d9;
+  width: 100%;
+  height: 100vh;
+  max-width: 646px;
+`;
+
+export const LimitedContainer = styled.div`
+  width: 100%;
+  max-width: 498px;
+  display: flex;
+  justify-content: center;
 `;
